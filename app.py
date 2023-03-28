58bec4a91bf54d45ad8ee4c3aa07828077acb1f9
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-03-28 13:18:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ import streamlit as st
 
 
 # loading the saved model 
-loaded_model = pickle.load(open("trained_model.sav",'rb'))
+# loaded_model = pickle.load(open("trained_model.sav",'rb'))
+
+temp = open("trained_model.sav","rb")
+loaded_model=pickle.load(temp)
 
 # creating a function for prediction 
 
