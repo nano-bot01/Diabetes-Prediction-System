c1e91e00fea8d4e0538bf87f8474ae0aa63afd88
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-03-28 13:10:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 
 # loading the saved model 
-loaded_model = pickle.load(open("C:/Users/ankit/.vscode/vscode/ML/Dibeties/trained_model.sav",'rb'))
+loaded_model = pickle.load(open("trained_model.sav",'rb'))
 
 # creating a function for prediction 
 
