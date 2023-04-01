1ab8056888941043b92139233f472b18e59feae7
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-04-01 19:16:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import pickle
 import streamlit as st
 
 # loading the saved model 
-loaded_model = pickle.load(open("trained_model.sav",'rb'))
+loaded_model = pickle.load(open("trained_model.pkl",'rb'))
 
 # temp = open("trained_model.pkl","rb")
 # loaded_model=pickle.load(temp)
